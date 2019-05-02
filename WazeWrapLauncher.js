b73ba167efeee7e9825f8066a2caac4eeda826f9
justins83 <justins83.waze@gmail.com>
2019-05-02 13:00:45 -0400

    Create WazeWrapLauncher.js
    Initial import of WW launcher

---
+++ WazeWrapLauncher.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         WazeWrap
+// @namespace    https://greasyfork.org/users/30701-justins83-waze
+// @version      2019.05.02.01
+// @description  Launcher for the WazeWrap library
+// @author       JustinS83/MapOMatic
+// @include      https://beta.waze.com/*editor*
+// @include      https://www.waze.com/*editor*
+// @exclude      https://www.waze.com/*user/editor/*
+// @grant        none
+// ==/UserScript==
+
+/* global W */
+/* global WazeWrap */
+/* global $ */
+/* jshint esversion:6 */
+
+var WazeWrap = {};
+
+(function() {
+    'use strict';
+
+    let minVersion = "2019.05.01.01";
+    
+    function bootstrap(tries = 1) {
+        if (W && W.map &&
+            W.model && W.loginManager.user &&
+            $)
+            init();
+        else if (tries < 1000)
+            setTimeout(function () { bootstrap(tries++); }, 100);
+        else
+            console.log('WazeWrap failed to load');
+    }
+    
+    bootstrap();
+    
+    async function init(){
+        if(typeof unsafeWindow !== 'undefined'){
+            if(unsafeWindow.WazeWrap && (!unsaveWindow.WazeWrap.Version || unsaveWindow.WazeWrap.Version > minVersion)){
+                window.WazeWrap = unsafeWindow.WazeWrap;
+                WazeWrap = window.WazeWrap;
+            }
+            else{
+                unsafeWindow.WazeWrap = WazeWrap;
+                window.WazeWrap = WazeWrap;
+                await $.getScript("https://greasyfork.org/scripts/24870-wazewrapbeta/code/WazeWrapBeta.js");
+            }
+        }
+        else{
+            if(window.WazeWrap && (!window.WazeWrap.Version || window.WazeWrap.Version > minVersion))
+                WazeWrap = window.WazeWrap;
+            else{
+                window.WazeWrap = WazeWrap;
+                await $.getScript("https://greasyfork.org/scripts/24870-wazewrapbeta/code/WazeWrapBeta.js");
+            }
+        }
+    }
+    
+})();
