90956829f020608d055856c0a3b5cd575f0e0376
justins83 <justins83.waze@gmail.com>
2019-06-06 16:46:54 -0400

    Update WazeWrapLib.js

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -26,7 +26,7 @@
 
     function init() {
         console.log("WazeWrap initializing...");
-        WazeWrap.Version = "2019.06.06.05";
+        WazeWrap.Version = "2019.06.06.06";
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 		
 	loadSettings();
