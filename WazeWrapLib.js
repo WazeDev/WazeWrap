20b03e109ba9ac14ed32848c7b8f01aec45c8936
justins83 <justins83.waze@gmail.com>
2020-09-10 10:08:01 -0400

    Update WazeWrapLib.js
    Adding Icon class

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -27,7 +27,7 @@
 
     async function init() {
         console.log("WazeWrap initializing...");
-        WazeWrap.Version = "2020.09.09.01";
+        WazeWrap.Version = "2020.09.10.01";
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 		
 	loadSettings();
@@ -1208,7 +1208,47 @@
             },
             CLASS_NAME: "W.DivIcon"
         });
-    }
+    
+		this.Icon = OpenLayers.Class({
+            url: null,
+            size: null,
+            offset: null,
+            calculateOffset: null,
+            imageDiv: null,
+            px: null,
+            initialize: function(a,b,c,d){
+                this.url=a;
+                this.size=b||{w: 20,h: 20};
+                this.offset=c||{x: -(this.size.w/2),y: -(this.size.h/2)};
+                this.calculateOffset=d;
+                a = OpenLayers.Util.createUniqueID("OL_Icon_");
+                var div = this.imageDiv = OpenLayers.Util.createAlphaImageDiv(a);
+                $(div.firstChild).removeClass('olAlphaImg');   // LEAVE THIS LINE TO PREVENT WME-HARDHATS SCRIPT FROM TURNING ALL ICONS INTO HARDHAT WAZERS --MAPOMATIC
+            },
+            destroy: function(){ this.erase();OpenLayers.Event.stopObservingElement(this.imageDiv.firstChild);this.imageDiv.innerHTML="";this.imageDiv=null; },
+            clone: function(){ return new OpenLayers.Icon(this.url,this.size,this.offset,this.calculateOffset); },
+            setSize: function(a){ null!==a&&(this.size=a); this.draw(); },
+            setUrl: function(a){ null!==a&&(this.url=a); this.draw(); },
+            draw: function(a){
+                OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,null,this.size,this.url,"absolute");
+                this.moveTo(a);
+                return this.imageDiv;
+            },
+            erase: function(){ null!==this.imageDiv&&null!==this.imageDiv.parentNode && OpenLayers.Element.remove(this.imageDiv); },
+            setOpacity: function(a){ OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,null,null,null,null,null,null,a); },
+            moveTo: function(a){
+                null!==a&&(this.px=a);
+                null!==this.imageDiv&&(null===this.px?this.display(!1): (
+                    this.calculateOffset&&(this.offset=this.calculateOffset(this.size)),
+                    OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,{x: this.px.x+this.offset.x,y: this.px.y+this.offset.y})
+                ));
+            },
+            display: function(a){ this.imageDiv.style.display=a?"": "none"; },
+            isDrawn: function(){ return this.imageDiv&&this.imageDiv.parentNode&&11!=this.imageDiv.parentNode.nodeType; },
+            CLASS_NAME: "OpenLayers.Icon"
+        });
+
+	}
 
     function Util() {
         /**
