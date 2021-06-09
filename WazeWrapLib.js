0941b13622bd570bea0572b218a46f320d1d8153
justins83 <justins83.waze@gmail.com>
2021-06-09 13:45:38 -0400

    Update WazeWrapLib.js

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -27,7 +27,7 @@
 
     async function init() {
         console.log("WazeWrap initializing...");
-        WazeWrap.Version = "2021.06.09.01";
+        WazeWrap.Version = "2021.06.09.02";
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 		
 	loadSettings();
@@ -260,7 +260,7 @@
             $('.WWAlertsHistory').css('top', `${toastrSettings.historyTopLoc}px`);
 
             try {
-                await $.getScript("https://greasyfork.org/scripts/28687-jquery-ui-1-11-4-custom-min-js/code/jquery-ui-1114customminjs.js");
+                await $.getScript("https://greasyfork.org/scripts/427721-jquery-ui-1-11-4-custom-min-js/code/jquery%20ui%201-11-4%20custom%20min%20js.js");
             }
             catch (err) {
                 console.log("Could not load jQuery UI " + err);
