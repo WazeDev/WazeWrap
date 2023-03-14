0bb097f075c8fa17301b5c7379fc17923787b8d9
justins83 <justins83.waze@gmail.com>
2023-03-14 14:17:46 -0400

    Update WazeWrapBeta.js

--- WazeWrapBeta.js
+++ WazeWrapBeta.js
@@ -19,7 +19,7 @@ var WazeWrap = {};
 (function() {
     'use strict';
     const MIN_VERSION = '2019.05.01.01';
-    const WW_URL = 'https://cdn.jsdelivr.net/gh/WazeDev/WazeWrap@beta/WazeWrapLib.js';
+    const WW_URL = 'https://cdn.jsdelivr.net/gh/WazeDev/WazeWrap@2023.03.14.01/WazeWrapLib.js';
 
     async function init(){
         const sandboxed = typeof unsafeWindow !== 'undefined';
