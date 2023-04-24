4e9edc555963885a1a0b127e9ff77ae2f84b1871
MapOMatic <mapomatic.waze@gmail.com>
2023-04-24 00:15:39 -0400

    bump version #

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -27,7 +27,7 @@
 
     async function init() {
         console.log("WazeWrap initializing...");
-        WazeWrap.Version = "2023.03.29.01";
+        WazeWrap.Version = "2023.04.24.01";
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 		
 	loadSettings();
