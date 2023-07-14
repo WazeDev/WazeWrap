57d7d04ce364598d57f6e222c8a1a3f55ced8f5b
justins83 <justins83.waze@gmail.com>
2023-07-14 09:45:02 -0400

    Update WazeWrapLib.js

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -27,7 +27,7 @@
 
     async function init() {
         console.log("WazeWrap initializing...");
-        WazeWrap.Version = "2023.07.13.03";
+        WazeWrap.Version = "2023.07.14.01";
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 		
 	loadSettings();
@@ -62,9 +62,9 @@
 			//inject functions for pulling information since WME backend is receiving frequent changes
 			arr.forEach((item, index, array) => { 
 				array[index].WW = {};
-				array[index].WW.getObjectModel = function(){ return this.attributes.wazeFeature._wmeObject;};
-				array[index].WW.getType = function(){return this.getObjectModel().type;}; 
-				array[index].WW.getAttributes = function(){return this.getObjectModel().attributes;}; 
+				array[index].WW.getObjectModel = function(){ return item.attributes.wazeFeature._wmeObject;};
+				array[index].WW.getType = function(){return item.WW.getObjectModel().type;}; 
+				array[index].WW.getAttributes = function(){return item.WW.getObjectModel().attributes;}; 
 			});
             return arr;
         };
