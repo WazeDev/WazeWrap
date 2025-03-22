1ee5eaa389c7a0ed80b26bbaef512b2671ff215e
MapOMatic <mapomatic.waze@gmail.com>
2025-03-22 06:01:36 -0400

    Bump WazeWrap version to 2025.03.12.01 for testing

--- WazeWrapLib.js
+++ WazeWrapLib.js
@@ -27,7 +27,7 @@
 
     async function init() {
         console.log("WazeWrap initializing...");
-        WazeWrap.Version = "2025.03.12.00";
+        WazeWrap.Version = "2025.03.12.01";
         WazeWrap.isBetaEditor = /beta/.test(location.href);
 		
 	loadSettings();
